ef559755639116dde04668b39350d5be966a4ea4
postemp <apostemp@mail.ru>
2023-07-10 23:10:18 +0300

    init

---
+++ src/main/java/HomeWork4.java
@@ -0,0 +1,16 @@
+public class HomeWork4 {
+    public static void main(String[] args) {
+        greetings("Hello");
+        greetings("World");
+        greetings("from");
+        greetings("Java");
+    }
+
+    public static void greetings(String word) {
+        System.out.println(word);
+    }
+
+    public static void checkSign(int a, int b, int c) {
+        int d = a + b + c;
+    }
+}
