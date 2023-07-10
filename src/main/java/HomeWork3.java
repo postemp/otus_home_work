04250cc8ae5c176e29d6458e8e57ff4f228bb7ed
postemp <apostemp@mail.ru>
2023-07-10 23:38:38 +0300

    first

---
+++ src/main/java/HomeWork3.java
@@ -0,0 +1,17 @@
+public class HomeWork3 {
+
+        public static void main(String[] args) {
+            greetings("Hello");
+            greetings("World");
+            greetings("from");
+            greetings("Java");
+        }
+
+        public static void greetings(String word) {
+            System.out.println(word);
+        }
+
+        public static void checkSign(int a, int b, int c) {
+            int d = a + b + c;
+        }
+}
--- src/main/java/HomeWork4.java
+++
@@ -1,6 +0,0 @@
-public class HomeWork4 {
-    public static void main(String[] args) {
-
-    }
-
-}
