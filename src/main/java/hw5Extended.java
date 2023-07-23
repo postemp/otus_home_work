5307a03dea68d27b6cad65a536d3390832c1368f
postemp <apostemp@mail.ru>
2023-07-23 23:45:29 +0300

    homeWork5.1

--- src/main/java/hw5Extended.java
+++ src/main/java/hw5Extended.java
@@ -1,2 +1,127 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class hw5Extended {
+    public static void main(String[] args) {
+
+        // 1 задание Cуммирование элементов в массиве
+        int[][] multiDimensionArr = new int[][]{
+                {1, 2, 3, 4}
+                , {2, 2}
+                , {1, 1, 1, 1, 1}
+                , {2}
+        };
+        System.out.println(Arrays.toString(sumArrayElements(multiDimensionArr)));
+
+        // 2 задание Найти середину между элементами массива
+        System.out.println(findingMiddle(new int[]{1, 1, 1, 3}));
+
+        // 3 задание Проверить, что все элементы идут в порядке убывания или возрастания
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Вы хотите проверить массив на убывание (введите 1) или возрастание? (введите 2)");
+        int whatToDo = scanner.nextInt();
+        int[] array = new int[] {4,2,1,-2,-10};
+        System.out.println( Arrays.toString(array) +"  "+ checkingIfIncreasing(array,whatToDo ));
+
+        // 4 задание Переворачиваем массив
+        System.out.println("Переворачиваем массив "+Arrays.toString(revertArr(new int[]{1, 2, 3, 4, 5, 6, -10})));
+    }
+
+    public static int[] sumArrayElements(int[][] multiDimArr) {
+        int[] arrOfSum = new int[0];
+        int tempSum;
+        int counter = 0;
+
+        while (true) {
+            tempSum = 0;
+            for (int j = 0; j < multiDimArr.length; j++) {
+                if (multiDimArr[j].length <= counter) {
+                    continue;
+                }
+//                System.out.print(multiDimArr[j][counter]+ " ");
+                tempSum += multiDimArr[j][counter];
+            }
+//            System.out.println(" Sum = "+ tempSum);
+            if (tempSum == 0) {
+                break;
+            }
+            // call the method to add tempSum in arr
+            arrOfSum = addX(counter, arrOfSum, tempSum);
+            counter++;
+        }
+        return arrOfSum;
+    }
+
+    // этот метод я нашел в интернете:
+    public static int[] addX(int n, int arr[], int x) {
+        int i;
+
+        // create a new array of size n+1
+        int newarr[] = new int[n + 1];
+
+        // insert the elements from
+        // the old array into the new array
+        // insert all elements till n
+        // then insert x at n+1
+        for (i = 0; i < n; i++)
+            newarr[i] = arr[i];
+
+        newarr[n] = x;
+
+        return newarr;
+    }
+
+    public static String findingMiddle(int[] arr) {
+        return "Пока не решил задание";
+    }
+
+    public static String checkingIfIncreasing(int[] arr, int flag) {
+        if (flag == 2) { // проверяем, что массив возрастает
+            int oldVal = Integer.MIN_VALUE;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < oldVal) {
+                    return "Массив не возрастает";
+                }
+                oldVal = arr[i];
+            }
+            return "Массив возрастает";
+        } else if (flag == 1) {  // проверяем, что массив убывает
+            int oldValue = Integer.MAX_VALUE;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] > oldValue) {
+                    return "Массив не убывает";
+                }
+                oldValue = arr[i];
+            }
+            return "Массив убывает";
+        } else {
+            return "Введите цифру 1 или 2";
+        }
+    }
+
+    public static int[] revertArr(int[] arr) {
+        int maxVal;
+        int minVal;
+        int arrRevCounter = arr.length-1;
+        for (int i = 0; i < arr.length; i++) {
+            if (i >= arrRevCounter) {break;}
+            minVal = arr[i];
+            maxVal = arr[arrRevCounter];
+            arr[i] = maxVal;
+            arr[arrRevCounter] = minVal;
+            arrRevCounter--;
+//            System.out.println( Arrays.toString(arr));
+        }
+        return  arr;
+//
+//        int tempVal = arr.length-1;
+//        int[] arrOut = new int[arr.length];
+//        for (int i = 0; i < arr.length; i++) {
+//            arrOut[i] = arr[tempVal];
+//            tempVal --;
+//        }
+//        return arrOut;
+    }
 }
+
+//1 4 5 1 2 2
--- src/main/java/hw5Simple.java
+++ src/main/java/hw5Simple.java
@@ -10,6 +10,7 @@ public class hw5Simple {
         fillEveryElement(4, new int[6]);
         int[] arr = {2, 4, 2, 4};
         increaseEveryElement(3, arr);
+        whichHalfIsGreater(new int[] {5,1,1,1,2,2,2,2} );
     }
 
     public static void printString(int counter, String string) {
@@ -42,5 +43,29 @@ public class hw5Simple {
             arr[i] += number;
         }
         System.out.println(Arrays.toString(arr));
+    }
+
+    public static void whichHalfIsGreater(int[] arr){
+        if (arr.length%2 != 0){
+            System.out.println("половины массива имеют разное количество чисел");
+            return;
+        }
+        int lestSideSum = 0;
+        int rightSideSum = 0;
+        // суммируем левую сторону массива
+        for (int i = 0; i < arr.length/2; i++) {
+            lestSideSum += arr[i];
+        };
+        // суммируем правую сторону массива
+        for (int i = arr.length/2; i < arr.length; i++) {
+            rightSideSum += arr[i];
+        };
+        if (lestSideSum > rightSideSum) {
+            System.out.println("Сумма левой стороны больше");
+        } else if (lestSideSum < rightSideSum) {
+            System.out.println("Сумма правой стороны больше");
+        } else {
+            System.out.println("Суммы сторон одинаковы");
+        }
     }
 }
