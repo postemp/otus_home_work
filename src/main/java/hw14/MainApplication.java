507034c0d3d53857e015db53367480aca508e882
postemp <apostemp@mail.ru>
2023-08-18 21:47:16 +0300

    homeWork 14

--- src/main/java/hw14/MainApplication.java
+++ src/main/java/hw14/MainApplication.java
@@ -6,7 +6,7 @@ public class MainApplication {
 //            workWithArr(new String[][]{{"1","2","3","4"},{"1","2","3","4"},{"1","2","3","4"},{"1","2","3","4"}}); // правильный массив
 //            workWithArr(new String[][]{{"1","2","3","4"},{"1","2","3","4","5"},{"1","2","3","4"},{"1","2","3","4"},{"2"}}); // размер неправильный
 //            workWithArr(new String[][]{{"1","2","3","4"},{"1  ","2","3","4"},{"1","2","3","4"},{"1","2","3","4"}}); // стоит лишний пробел рядом с цифрой
-            sumArrElements(new String[][]{{"1","2","3","4"},{"1","2","3","4","5"},{"1","2","3","4"},{"1","2","3","4"}});  // неправильное количество элементов во 2 элементе
+            sumArrElements(new String[][]{{"1", "2", "3", "4"}, {"1", "2", "3", "4", "5"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}});  // неправильное количество элементов во 2 элементе
         } catch (AppArrayDataException e) {
 //            e.printStackTrace();
             System.out.println(e.getMessage());
@@ -17,12 +17,12 @@ public class MainApplication {
     }
 
     public static void sumArrElements(String[][] twoDimArr) {
-        if (twoDimArr.length != 4 ){
-            throw new AppArraySizeException("Ошибка в размере массива length = "+ twoDimArr.length);
+        if (twoDimArr.length != 4) {
+            throw new AppArraySizeException("Ошибка в размере массива length = " + twoDimArr.length);
         }
         for (int i = 0; i < twoDimArr.length; i++) {
             if (twoDimArr[i].length != 4) {
-                throw new AppArraySizeException("Ошибка в размере массива [" + i + "].length = "+ twoDimArr[i].length);
+                throw new AppArraySizeException("Ошибка в размере массива [" + i + "].length = " + twoDimArr[i].length);
             }
         }
 
@@ -32,10 +32,10 @@ public class MainApplication {
                 try {
                     sum += Integer.parseInt(twoDimArr[i][j]);
                 } catch (NumberFormatException e) {
-                    throw new AppArrayDataException("словили excepton NumberFormatException","проблема в ячейке ["+i+"][" + j +"]");
+                    throw new AppArrayDataException("словили excepton NumberFormatException", "проблема в ячейке [" + i + "][" + j + "]");
                 }
             }
         }
-        System.out.println("Сумма = "+ sum);
+        System.out.println("Сумма = " + sum);
     }
 }
