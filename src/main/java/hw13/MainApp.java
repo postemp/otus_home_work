a141aedda7e0a1ad76464be2f6bc0477e2f0fa42
postemp <apostemp@mail.ru>
2023-08-26 05:50:57 +0300

    homeWork 13

--- src/main/java/hw13/MainApp.java
+++ src/main/java/hw13/MainApp.java
@@ -1,32 +1,47 @@
 package hw13;
 
-import hw13.transport.Car;
-import hw13.transport.Man;
-import hw13.transport.Terrain;
+import hw13.transport.*;
 
 public class MainApp {
     public static void main(String[] args) {
 
-        Car car = new Car(40);
-        System.out.println("Запас хода " + car.getRangeOf() + " km");
+//        Car car = new Car(40);
+//        System.out.println("Запас хода " + car.getRangeOf() + " km");
+
         Man vasya = new Man("Вася", 40);
         vasya.transportInfo();
-
-        vasya.go(20, Terrain.FOREST);
-
-        hw13.transport.AllTerrainVehicle ATV =new hw13.transport.AllTerrainVehicle(60);
-
-        vasya.getOn(ATV);
-        vasya.go(20, Terrain.FOREST);
-
+        vasya.go(20,Terrain.SWAMP);
         vasya.transportInfo();
-        vasya.getOn(car);
 
-        System.out.println(vasya.go(40, Terrain.PLAIN));
-        car.showLeftRangeOf();
-
+        Bicycle bike = new Bicycle(60);
+        vasya.getOn(bike);
+        vasya.go(10,Terrain.PLAIN);
         vasya.getOff();
-        System.out.println(vasya.go(4, Terrain.PLAIN));
+        vasya.transportInfo();
+
+//        Horse sivka = new Horse(200);
+//        vasya.getOn(sivka);
+//        vasya.transportInfo();
+//        vasya.go(100,Terrain.SWAMP);
+
+
+//
+//        vasya.go(20, Terrain.FOREST);
+//        vasya.transportInfo();
+//        hw13.transport.AllTerrainVehicle ATV =new hw13.transport.AllTerrainVehicle(60);
+//
+//        vasya.getOn(ATV);
+//        vasya.transportInfo();
+//        vasya.go(20, Terrain.FOREST);
+//
+//        vasya.transportInfo();
+//        vasya.getOn(car);
+//
+//        System.out.println(vasya.go(40, Terrain.PLAIN));
+//        car.showLeftRangeOf();
+//
+//        vasya.getOff();
+//        System.out.println(vasya.go(4, Terrain.PLAIN));
 
     }
 }
