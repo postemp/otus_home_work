a7907c7769a7ce81777ffe0044999cd15311445d
postemp <apostemp@mail.ru>
2023-08-31 07:29:07 +0300

    homeWork 16

--- src/main/java/hw16/phoneBook/PhoneBook.java
+++ src/main/java/hw16/phoneBook/PhoneBook.java
@@ -1,29 +1,22 @@
 package hw16.phoneBook;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PhoneBook {
-    private final Map<String, ArrayList<String>> phoneBook = new HashMap<>();
+    private final Map<String, HashSet<String>> phoneBook = new HashMap<>();
 
     public PhoneBook() {
     }
 
     public void add(String name, String phone) {
         if (phoneBook.containsKey(name)) {
-            if (phoneBook.get(name).contains(phone)) {
-                System.out.println("Такой номер у " + name + " уже записан");
-                return;
-            }
-            phoneBook.get(name).add(phone);
+            System.out.println("записываем номер " + phone +" у " + name + " результат: " +phoneBook.get(name).add(phone));
         } else {
-            phoneBook.put(name, new ArrayList<>(List.of(phone)));
+            phoneBook.put(name, new HashSet<>(Set.of(phone)));
         }
     }
 
-    public List<String> find(String name) {
+    public Set<String> find(String name) {
 //        if (!phoneBook.containsKey(name)) {
 //            System.out.println("имени \"" +name+ "\" в записной книге нет");
 //            throw new RuntimeException("Ошибка, имени \"" +name+ "\" в записной книге нет");
@@ -32,8 +25,8 @@ public class PhoneBook {
     }
 
     public boolean containsPhoneNumber(String phone) {
-        for (Map.Entry<String, ArrayList<String>> set : phoneBook.entrySet()) {
-            for (Object o : set.getValue()) {
+        for (Map.Entry<String, HashSet<String>> set1 : phoneBook.entrySet()) {
+            for (Object o : set1.getValue()) {
                 if (o.toString().equals(phone)) {
                     return true;
                 }
