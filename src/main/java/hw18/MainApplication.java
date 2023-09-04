fd701ceab1f351b68bd6c9171a15dd900adf476d
postemp <apostemp@mail.ru>
2023-09-04 17:01:24 +0300

    homeWork 18

--- src/main/java/hw18/MainApplication.java
+++ src/main/java/hw18/MainApplication.java
@@ -8,13 +8,12 @@ import java.util.List;
 public class MainApplication {
     // список найденных текстовых файлов
     static List<File> fileList = new ArrayList<>();
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         File file = new File("./src");
         // ищем текстовые файлы,что бы их показать
         textFilesList(file);
-        System.out.println(fileList);
-
-        System.out.println("Введите имя файла, который вы хотите прочитать:"  );
+        System.out.println("Найдены файлы: "+fileList);
+        System.out.println("Введите имя файла из этого списка, который вы хотите прочитать:"  );
         String fileToReadString = readFromConsole();
         System.out.println("Показываем содержимое файла " +fileToReadString+ " в консоли:");
         File fileToReadWrite = new File(fileToReadString);
@@ -22,13 +21,13 @@ public class MainApplication {
             System.out.println("Такой файл не найден");
             return;
         }
-        System.out.println("Напечтайте строку, которую вы хотите добавить в файл");
+        System.out.println("Напечaтайте строку, которую вы хотите добавить в файл");
         String whatToWriteString = readFromConsole();
         if (writeStringToFile(fileToReadWrite, whatToWriteString)) {
             System.out.println("Файл успешно записан");
         } else {
             System.out.println("Произошла ошибка при записи файла");
-        };
+        }
     }
 
     public static void textFilesList(File path) {
@@ -70,10 +69,9 @@ public class MainApplication {
         return true;
     }
 
-
     public static boolean writeStringToFile(File fileToWrite, String whatToWrite) {
         whatToWrite = "\n"+whatToWrite;
-        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(fileToWrite, true))) {
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(fileToWrite, true))){
             byte[] buffer = whatToWrite.getBytes(StandardCharsets.UTF_8);
             for (int i = 0; i < buffer.length; i++) {
                 out.write(buffer[i]);
