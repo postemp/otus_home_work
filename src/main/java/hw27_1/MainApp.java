ad28b9c76958a7105f170d8518ac732039424430
postemp <apostemp@mail.ru>
2023-10-08 20:24:46 +0300

    hw27_1

---
+++ src/main/java/hw27_1/MainApp.java
@@ -0,0 +1,41 @@
+package hw27;
+
+
+import hw27.rwMethods.ReadWriteMethods;
+
+import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) throws IOException {
+
+        // ищем текстовые файлы,что бы их показать
+        File file = new File("./src");
+        System.out.println("Найдены файлы: ");
+        for (File fileName : ReadWriteMethods.textFilesList(file)) {
+            System.out.println(fileName.getPath());
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите имя файла из этого списка, который вы хотите прочитать:");
+        String fileToReadString = sc.nextLine();
+//        String fileToReadString = "./src/main/java/hw27/textFiles/1.txt";
+        System.out.println("Введите последовательность символов, которые вы хотите найти и подсчитать их количество (например \"журнал\")");
+        String symbolsToFindAndCount = sc.nextLine();
+
+        System.out.println("Показываем содержимое файла " + fileToReadString + " в консоли:");
+        File fileToReadWrite = new File(fileToReadString);
+        try {
+            ReadWriteMethods.findPattern(fileToReadWrite, symbolsToFindAndCount);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            sc.close();
+            return;
+        }
+
+    }
+
+
+}
---
+++ src/main/java/hw27_1/rwMethods/ReadWriteMethods.java
@@ -0,0 +1,59 @@
+package hw27.rwMethods;
+
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReadWriteMethods {
+    // список найденных текстовых файлов
+    static private final List<File> fileList = new ArrayList<>();
+
+    public static void searchFiles(File path) {
+        if (path.isFile() && path.getPath().contains(".txt")) {
+            fileList.add(path);
+        } else if (path.isDirectory()) {
+            for (File oneFile : path.listFiles()) {
+                searchFiles(oneFile);
+            }
+        }
+    }
+
+    public static List<File> textFilesList(File path) {
+        searchFiles(path);
+        return fileList;
+    }
+
+    public static void findPattern(File file, String patternString) throws IOException {
+        if (!file.exists()) {
+            throw new FileNotFoundException("Файл не найден");
+        }
+        Pattern pattern = Pattern.compile(patternString);
+
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(file.getPath()));
+            String line = reader.readLine();
+            int matches = 0;
+            while (line != null) {
+                Matcher matcher = pattern.matcher(line);
+                while (matcher.find()){
+                    matches++;
+                }
+//                System.out.println(line);
+                line = reader.readLine();
+            }
+            reader.close();
+            System.out.println("matches="+matches);
+
+            System.out.println();
+        } catch (
+                java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
