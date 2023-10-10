c9c9432cb8e05f36a72e57490185d2c393939ada
postemp <apostemp@mail.ru>
2023-10-10 23:19:28 +0300

    homeWork 26

--- src/main/java/hw26/utils/Apple.java
+++ src/main/java/hw26/utils/Apple.java
@@ -1,8 +1,9 @@
 package hw26.utils;
 
-public class Apple extends Fruit{
+public class Apple extends Fruit {
     private double weight = 20;
 
+    @Override
     public double getWeight() {
         return this.weight;
     }
--- src/main/java/hw26/utils/Orange.java
+++ src/main/java/hw26/utils/Orange.java
@@ -3,6 +3,7 @@ package hw26.utils;
 public class Orange extends Fruit {
     private double weight = 30;
 
+    @Override
     public double getWeight() {
         return this.weight;
     }
